63e5c61a1555cc9921de3a57484099ad1dff831d
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2025-05-21 21:43:48 +0000

    Adicionar script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1> "Cada linha de código é uma peça na construção do seu futuro. Continue a programar, a aprender e a criar!" </h1>
+function Home() {
+  return (
+    <h1>
+      {" "}
+      "Cada linha de código é uma peça na construção do seu futuro. Continue a
+      programar, a aprender e a criar!"{" "}
+    </h1>
+  );
+}
+function teste1() {
+  console.log("teste indentação");
 }
 
-export default Home
+export default Home;
