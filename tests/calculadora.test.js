7624e7e661f30ff07cdb51959c1867c65cf67999
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2025-06-30 14:45:39 +0000

    testes de verdades

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,16 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("2 + 2 dever retorna 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("2 + 3 dever retorna 5", () => {
+  const resultado = calculadora.somar(2, 3);
+  expect(resultado).toBe(5);
+});
+
+test("banana + 3 dever retornar error", () => {
+  const resultado = calculadora.somar("banana", 3);
+  expect(resultado).toBe("error");
 });
