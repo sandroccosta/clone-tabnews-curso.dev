79219e1540433bdf9836a0e07bff2e84c9b0ca57
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2025-07-23 18:06:27 +0000

    adição dos arquivos databas.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+import Database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ status: "Estar tudo Funcionando" });
 }
 
