6451a6bbf0e87ffe98285b934d5b6a2f324d6ea5
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2025-08-26 04:20:22 +0000

    move `.env` file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,4 @@
 import database from "../../../../infra/database";
-import Database from "../../../../infra/database";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
